81980168b6a5d7c5c5e09d382332d308fb74e32d
Midi Nanakul <midinanakul@hotmail.com>
2022-01-10 03:47:00 -0800

    Added a quotes.txt file with lots of quotes. Added new variables to main script and weekday_check function.

--- motivational_monday.py
+++ motivational_monday.py
@@ -1,7 +1,19 @@
 import smtplib
 import json
 import getpass
+import datetime as dt
+import random
 
 email = getpass.getpass('Email: ')
 password = getpass.getpass('Password: ')
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+
+def weekday_check():
+    if weekday == 1:
+        with open('quotes.txt') as q:
+            all_quotes = q.readlines()
+            random_quote = random.choice(all_quotes)
 
