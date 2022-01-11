53608bb023b37c68a544d1adea7ea147744a9172
Midi Nanakul <midinanakul@hotmail.com>
2022-01-11 15:30:39 -0800

    Fixed main function by removing quotes. Removed json import.

--- motivational_monday.py
+++ motivational_monday.py
@@ -1,5 +1,4 @@
 import smtplib
-import json
 import getpass
 import datetime as dt
 import random
@@ -17,9 +16,9 @@ def weekday_check():
     if weekday == 1:
         with open('quotes.txt') as q:
             all_quotes = q.readlines()
-            random_quote = random.choice(all_quotes)
+            rand_quote = random.choice(all_quotes)
 
-    return random_quote
+    return rand_quote
 
 
 def send_email(_random_quote):
@@ -33,7 +32,7 @@ def send_email(_random_quote):
                             msg='Subject:' + subject + '\n' + random_quote)
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     random_quote = weekday_check()
 
     weekday_check()
