f4a37c34218308525e9e9bafec4b680a4dd9af6a
Midi Nanakul <midinanakul@hotmail.com>
2022-01-11 17:32:26 -0800

    Converted string to asciiformat.

--- motivational_monday.py
+++ motivational_monday.py
@@ -12,13 +12,15 @@ now = dt.datetime.now()
 weekday = now.weekday()
 
 
-def weekday_check():
-    if weekday == 1:
+def weekday_check() -> str:
+    if weekday == 0:
         with open('quotes.txt') as q:
             all_quotes = q.readlines()
             rand_quote = random.choice(all_quotes)
 
-    return rand_quote
+        return rand_quote
+    else:
+        pass
 
 
 def send_email(_random_quote):
@@ -29,7 +31,7 @@ def send_email(_random_quote):
         connection.login(email, password)
         connection.sendmail(email,
                             to_addrs=to_address,
-                            msg='Subject:' + subject + '\n' + random_quote)
+                            msg=('Subject:' + subject + '\n' + _random_quote).encode('ascii', 'ignore'))
 
 
 if __name__ == '__main__':
