d1f99de1ae8779a6cb2def1d2f0b8fd1a8df6ee9
Midi Nanakul <midinanakul@hotmail.com>
2022-01-13 00:58:20 -0800

    Added send_email function.

--- birthday_wisher.py
+++ birthday_wisher.py
@@ -7,14 +7,26 @@ today = (datetime.now().month, datetime.now().day)
 data = pd.read_csv('birthdays.csv')
 birthdays_d = {(data_row['month'], data_row['day']) : data_row for (index, data_row) in data.iterrows()}
 
-
-def check_replace():
-    """This function will check to see if today's date matches someone's birthday date."""
-    if today in birthdays_d:
-        special_person = birthdays_d[today]
-        file_path = './bday_letter1.txt'
-        with open(file_path) as f:
-            contents = f.read()
-            contents.replace('[NAME]', special_person['name'])
+# Check to see if today's date matches a birthday.
+if today in birthdays_d:
+    special_person = birthdays_d[today]
+    file_path = './bday_letter1.txt'
+    with open(file_path) as f:
+        contents = f.read()
+        contents = contents.replace('[NAME]', special_person['name'])
 
 
+def send_email():
+    my_email = getpass.getpass('Email: ')
+    my_password = getpass.getpass('Password: ')
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.ehlo()
+        connection.starttls()
+        connection.login(my_email, my_password)
+        connection.sendmail(my_email, special_person['email'],
+                            msg=f'Subject:Happy Birthday!! \n\n{contents}')
+
+
+if __name__ == '__main__':
+    send_email()
