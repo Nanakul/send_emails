dda61537392491f400f932f8ea5eeb9a6879f724
Midi Nanakul <midinanakul@hotmail.com>
2022-01-12 23:51:14 -0800

    Various variable name changes. Made email column in db UNIQUE.

--- motivational_monday.py
+++ motivational_monday.py
@@ -10,6 +10,7 @@ import sqlite3 as db
 # Log in with Gmail
 my_email = getpass.getpass('Email: ')
 my_password = getpass.getpass('Password: ')
+to_address = input('Email of recipient: ')
 subject = 'Motivational Monday!'
 
 now = dt.datetime.now()
@@ -18,7 +19,7 @@ weekday = now.weekday()
 # Connect the database
 db_conn = db.connect('Emails.db')
 cursor = db_conn.cursor()
-cursor.execute("""CREATE TABLE if NOT EXISTS Emails (email VARCHAR)""")
+cursor.execute("""CREATE TABLE if NOT EXISTS Emails (email VARCHAR UNIQUE)""")
 db_conn.commit()
 
 
@@ -33,30 +34,25 @@ def weekday_check() -> str:
         pass
 
 
-def send_email(_random_quote) -> str:
-
-    to_address = input('Email of recipient: ')
+def send_email(_random_quote):
 
     with smtplib.SMTP('smtp.gmail.com', 587) as connection:
         connection.ehlo()
         connection.starttls()
         connection.login(my_email, my_password)
-        connection.sendmail(email,
+        connection.sendmail(my_email,
                             to_addrs=to_address,
                             msg=('Subject:' + subject + '\n' + _random_quote).encode('ascii', 'ignore'))
 
-    return to_address
-
 
 def db_add_email(_to_address):
-    cursor.execute('INSERT INTO Emails(email) VALUES (?)', str(_to_address))
+    cursor.execute('INSERT or IGNORE INTO Emails (email) VALUES (?)', (_to_address,))
     db_conn.commit()
 
 
 if __name__ == '__main__':
     random_quote = weekday_check()
-    email = send_email(random_quote)
 
     weekday_check()
     send_email(random_quote)
-    db_add_email(email)
+    db_add_email(to_address)
